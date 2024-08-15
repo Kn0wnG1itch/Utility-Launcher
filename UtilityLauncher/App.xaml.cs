a012832b4b661b8fd6e473f94cad69677ca587ad
Kn0wnG1itch <119559352+Kn0wnG1itch@users.noreply.github.com>
2024-08-15 18:13:14 -0500

    Initial project setup and .gitignore configuration

---
+++ UtilityLauncher/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UtilityLauncher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
